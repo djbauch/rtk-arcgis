c20f8d6ed2b56d2a4c9b4701f29c76b1950fc81d
DJ Bauch <dj.bauch@gmail.com>
2023-02-15 11:22:27 -0600

    Added Accordion for HTML content

--- src/ArcGisQueryTest.tsx
+++ src/ArcGisQueryTest.tsx
@@ -5,17 +5,22 @@ import { DateTime } from "luxon";
 import Pagination from "@mui/material/Pagination";
 import Typography from "@mui/material/Typography";
 import Stack from "@mui/material/Stack";
-
+import Accordion from "@mui/material/Accordion";
+import AccordionSummary from "@mui/material/AccordionSummary";
+import AccordionDetails from "@mui/material/AccordionDetails";
+import ExpandMoreIcon from "@mui/icons-material/ExpandMore";
 import { useGetArcGisSearchResultByTopicQuery } from "./services/arcgisEnterprise";
 import { ArcGisQueryArgs } from "./services/types";
 
-const ArcGisQueryTest: React.FC<ArcGisQueryArgs> = ({topic}) => {
+const ArcGisQueryTest: React.FC<ArcGisQueryArgs> = ({ topic }) => {
   const [page, SetPage] = React.useState(1);
   const handleChange = (event: React.ChangeEvent<unknown>, value: number) => {
     SetPage(value);
   };
-  const { data, error, isLoading } =
-    useGetArcGisSearchResultByTopicQuery({topic, start: (page - 1) * 10 + 1});
+  const { data, error, isLoading } = useGetArcGisSearchResultByTopicQuery({
+    topic,
+    start: (page - 1) * 10 + 1,
+  });
 
   return (
     <div className="QueryResult">
@@ -26,7 +31,9 @@ const ArcGisQueryTest: React.FC<ArcGisQueryArgs> = ({topic}) => {
       ) : data ? (
         <>
           <Stack spacing={1}>
-            <Typography>Page: {page} of {Math.ceil(data.total / 10)}</Typography>
+            <Typography>
+              Page: {page} of {Math.ceil(data.total / 10)}
+            </Typography>
             <Pagination
               count={Math.ceil(data.total / 10)}
               page={page}
@@ -42,19 +49,28 @@ const ArcGisQueryTest: React.FC<ArcGisQueryArgs> = ({topic}) => {
             <p>Results:</p>
             {data.results.map((result) => (
               <div key={result.id}>
-
                 <p>ID: {result.id}</p>
                 <p>Owner: {result.owner}</p>
                 <p>Name: {result.name}</p>
+                <p>Title: {result.title}</p>
+                <p>Type: {result.type}</p>
                 <p>
                   Created: {DateTime.fromMillis(result.created).toISODate()}
                 </p>
                 <p>
                   Modified: {DateTime.fromMillis(result.modified).toISODate()}
                 </p>
-                <React.Fragment>
-                  {parse(sanitizeHtml(result.description))}
-                </React.Fragment>
+                <Accordion>
+                  <AccordionSummary expandIcon={<ExpandMoreIcon />}>
+                    <Typography>{result.snippet}</Typography>
+                  </AccordionSummary>
+                  <AccordionDetails>
+                    <React.Fragment>
+                      {parse(sanitizeHtml(result.description))}
+                    </React.Fragment>
+                  </AccordionDetails>
+                </Accordion>
+
                 <hr />
               </div>
             ))}
@@ -63,5 +79,5 @@ const ArcGisQueryTest: React.FC<ArcGisQueryArgs> = ({topic}) => {
       ) : null}
     </div>
   );
-}
-export default ArcGisQueryTest
+};
+export default ArcGisQueryTest;
